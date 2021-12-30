f2ace675639024f292d044bb0297ac1a6495b2e3
ujjal zaman <muzzwal97@gmail.com>
2021-12-30 22:33:25 +0600

    leads

--- backend/main.py
+++ backend/main.py
@@ -41,3 +41,7 @@ async def create_lead(lead: _schemas.LeadCreate,user:_schemas.User = _fastapi.De
 @app.get("/api/leads", response_model=List[_schemas.Lead])
 async def get_leads(user:_schemas.User=_fastapi.Depends(_services.get_current_user),db:_orm.Session = _fastapi.Depends(_services.get_db)):
     return await _services.get_leads(user=user, db=db)
+
+@app.get("/api/leads/{lead_id}", status_code=200)
+async def get_lead(lead_id:int,user:_schemas.User = _fastapi.Depends(_servicesget_current_user),db:_orm.Session=_fastapi.Depends(_services.get_db)):
+    return await _services.get_lead(lead_id, user, db)
