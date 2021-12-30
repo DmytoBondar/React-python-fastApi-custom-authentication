2b307fd96e24c384a468766868812507303e0046
ujjal zaman <muzzwal97@gmail.com>
2021-12-30 17:25:15 +0600

    added database

--- backend/services.py
+++ backend/services.py
@@ -1,9 +1,4 @@
-import sqlalchemy as _sql
-import sqlalchemy.ext.declarative as _declarative
-import sqlalchemy.orm as _orm
+import database as _database
 
-DATABASE_URL = "sqlite:///./database.db"
-
-engine = _sql.create_engine(DATABASE_URL, connect_args={"check_same_thread":False})
-
-Sessionalocal = _orm.sessionmaker(autocommit=False, autoflush=False,bind=engine)
+def create_database():
+    return _database.Base.metadata.create_all(bind=_database.engine)
